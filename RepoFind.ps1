6b736db55156a7c247a350ccd4544abe95ef6e4a
Bruce Hamilton <v-bruham@microsoft.com>
2017-09-25 23:38:26 -0700

    Add files via upload

--- RepoFind.ps1
+++ RepoFind.ps1
@@ -7,15 +7,14 @@
     [string]$LineNum
     [string]$Context
     [bool]$Multiple
-    [string]$Line
     [string]$Heading
  }
 
 $RFresults = New-Object "System.Collections.Generic.List[RFind]"
 $homeFolder = $HOME
 $rfDir = "";
-$curdir = (Get-Item -Path ".\" -Verbose).FullName
-$configFile = Join-Path -Path $curdir -ChildPath "rfconfig.xml"
+$curDir = (Get-Item -Path ".\" -Verbose).FullName
+$configFile = Join-Path -Path $curDir -ChildPath "rfconfig.xml"
 $sources = New-Object 'System.Collections.Generic.List[string]'
 if(Test-Path $configFile)
  {
@@ -41,35 +40,62 @@ if(Test-Path $configFile)
  }
 
 $srchstr = $args[0]
-$outfile = Join-Path -Path $curdir -ChildPath "rfresults.txt"
 $lasthead = ""
 
 Foreach ($sf in $sources)
 {
+    $spath = ""
     if ($rfDir -eq "")
     {
         $spath = Join-Path -Path $HOME -ChildPath $sf
+        $repStart = $HOME.Length + 1
     }
     else
     {
-        $spath = Join-Path -Path $rfDir -ChildPath $sf        
+        $spath = Join-Path -Path $rfDir -ChildPath $sf
+        $repStart = $rfDir.Length + 1        
     }
     $mdPth = "{0}\*.md" -f $spath
+    $repPath = $spath.Substring($repStart)
+
     $files = Get-ChildItem $mdPth -Recurse
     foreach ($file in $files) 
     {
         $filetxt = [System.IO.File]::ReadAllText($file)
         $ln = 1
-        if ($filetxt.Contains($srchstr))  
+        if ($filetxt.ToLower().Contains($srchstr.ToString().ToLower()))  
         {
             $content = Get-Content $file
             foreach($line in $content)
             {
-                if($line.Contains($srchstr))
+                if($line.ToLower().Contains($srchstr.ToString().ToLower()))
                 {
-                    $entry = "{0}`t{1}`t{2}`t{3}" -f $file,$ln,$line,$lasthead
                     $hit = New-Object "RFind"
-  
+
+                    # Construct the URL to published topic
+                    $idx = $repPath.IndexOf("\");
+                    $repo = $repPath.Substring(0,$idx)
+                    $hit.Repo = $repo
+                    $ridx = $file.FullName.IndexOf($repo);
+                    if ($repo -eq "sql-docs-pr")
+                    {
+                        $strt = $ridx + 17;
+                        $urlpart = "sql"
+                    }
+                    elseif ($repo -eq "azure-docs-pr")
+                    {
+                        $strt = $ridx + 23;
+                        $urlpart = "azure"
+                    }
+                    $urlx = $file.FullName.SubString($strt)
+                    $urlx = $urlx.Replace("\","/")
+                    $urlx = $urlx.TrimEnd('.','m','d')
+                    $msURL = "=HYPERLINK(`"https://docs.microsoft.com/{0}/{1}`")" -f $urlPart, $urlx
+                    $hit.URL = $msURL
+                    
+                    $hit.Multiple = $false
+                    
+
                     # get context, 40 chars before hit and after
                     $ix = $line.IndexOf($srchstr)
                     if ($ix -ge 40)
@@ -82,7 +108,7 @@ Foreach ($sf in $sources)
                         $alpha = $line.Substring(0,$ix)
                     }
                     $slen = $srchstr.ToString().Length
-                    if ($ix + $slen + 40 -gt $line.Length)
+                    if ($ix + $slen + 40 -ge $line.Length)
                     {
                         $rlen = $line.Length - ($ix + $slen)
                         $omega = $line.Substring($ix + $slen,$rlen)
@@ -95,20 +121,22 @@ Foreach ($sf in $sources)
                     $hit.Context = $context
 
                     $hit.FileName = [System.IO.Path]::GetFileName($file)
-                    $hit.Folder = [System.IO.Path]::GetDirectoryName($file)
-                    $hit.Line = $line
+                    $fldr = [System.IO.Path]::GetDirectoryName($file)
+                    $hit.Folder = Split-Path -Path $fldr -Leaf
                     $hit.LineNum = $ln
-                    $idx = $sf.IndexOf("\")
-                    $hit.Repo = $sf.Substring(0,$idx -1)
-                    $hit.URL = "MEOW"
+                    $hit.Heading = $lasthead
 
+                    if ($omega.Contains($srchstr))
+                    {
+                        $hit.Multiple = $true
+                    }
+ 
                     $RFresults.Add($hit)
 
-                    # Add-Content $outfile -Value $entry
                 }
                 if ($line.StartsWith("#"))
                 {
-                    $ix = $line.IndexOf(" #")
+                    $ix = $line.ToString().IndexOf("# ")
                     $lasthead = $line.Substring($ix + 1)
                 }
                 $ln++
@@ -117,14 +145,22 @@ Foreach ($sf in $sources)
     }
 }
 
-$RFresults | Export-Csv -Path c:\meowmeta\meow.csv -NoTypeInformation
-
-
-
-
-
-
-
-
-
+try
+{
+    if ($RFresults.Count -ge 0)
+    {
+        $csvFile = [System.IO.Path]::Combine($curDir, "RepoFindResults.csv")
+        $RFresults | Export-Csv -Path $csvFile -NoTypeInformation
+        $msg = "{0} occurences found. See RepoFindResults.csv." -f $RFresults.Count
+        Write-Host $msg
+    }
+    else
+    {
+        Write-Host "No search results found."
+    }
+}
+Catch [System.IO.IOException]
+{
+    Write-Host "RFresults.csv is open, please close it."
+}
 
